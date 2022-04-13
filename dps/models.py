863cb590cb3a85cffdfcd991852c4f39ddfac6ab
Rafael Torres <8873246+paengski13@users.noreply.github.com>
2022-04-13 21:18:58 +1200

    django3 support

--- dps/models.py
+++ dps/models.py
@@ -7,7 +7,7 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from six import python_2_unicode_compatible
-from django.utils.six import text_type
+from six import text_type
 
 
 def make_uuid():
